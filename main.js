df400491f1c392096c51fd95f11df2f8e156e638
NoemiPast <lic.noemipastrana@gmail.com>
2025-05-21 17:38:43 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ const consumirApi = () => {
                 const suite = document.getElementById("suite");
                 const city = document.getElementById("city");
                 const zipcode = document.getElementById("zipcode");
+            const contenedor = document.getElementById("contenedor");
 
             //data es un array/arreglo, por eso el [0]
             nombre.innerText = data[6].name
@@ -29,7 +30,26 @@ const consumirApi = () => {
                 suite.innerText = data[6].address.suite
                 city.innerText = data[6].address.city
                 zipcode.innerText = data[6].address.zipcode
+                zipcode.innerHTML =  `<h1>${data[6].address.zipcode}</h1>`; 
+                //innerHTML me permite agregar etiquetas HTML. 
 
+                /*Aqui vamos a facilitarnos la vida con un forEach*/
+                data.forEach(
+                    (usuario, index) => //El "usuario" puede ser cualquier otra palabra, no necesariamente "usuario"
+                        (contenedor.innerHTML += `
+                            <p>${usuario.name}</p>
+                            <p>${usuario.username}</p>
+                            <p>${usuario.email}</p>
+                            <p>${usuario.phone}</p>
+                            <p>${usuario.website}</p>
+                            <p>${usuario.address.street}</p>
+                            <p>${usuario.address.suite}</p>
+                            <p>${usuario.address.city}</p>
+                            <p>${usuario.address.zipcode}</p> 
+                            <hr> //En HTML es una etiqueta que me permite separar los elementos                                                         
+                            `)
+
+                );
        }) 
         //Descubrir que hacer en caso de que no me corresponda
         .catch((error) => console.log(error));
